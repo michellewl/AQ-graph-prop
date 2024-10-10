5ec180d5b5fcacc93be8f7113dc9747689ea052a
Michelle WL Wan <54372792+michellewl@users.noreply.github.com>
2024-10-10 16:43:06 +0100

    Multi species propagation in the random seed exps. Still some bugs?

--- random_seed_exps.py
+++ random_seed_exps.py
@@ -5,21 +5,43 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from graph_utils import *
 
-species = "PM10"
+propagation_approach = "single" # "multi"
+
+species = "NO2" # gaps will be introduced into this species
+
+# Create a folder for imputation results
+species_folder = f"{propagation_approach}_{species}"
+output_folder = path.join("results", species_folder)
+makedirs(output_folder, exist_ok=True)  # Create folder if it doesn't exist
+
+# Load data
 region = "London"
 start_date = "1996-01-01"
 end_date = "2021-01-01"
 data_folder = "/Users/michellewan/Library/CloudStorage/OneDrive-UniversityofCambridge/Documents/PhD/MEng_Kevin/Data and code"
-data_filename = f"LAQN_{species}_{start_date}_{end_date}.csv"
 
-# Create a folder for single species propagation
-species_folder = f"single_{species}"
-output_folder = path.join("results", species_folder)
-makedirs(output_folder, exist_ok=True)  # Create folder if it doesn't exist
+dfs = []
 
-data = Dataset(path.join(data_folder, data_filename))
+if propagation_approach == "multi" or (propagation_approach=="single" and species=="NO2"):
+    # Load NO2 data
+    data_filename = f"LAQN_NO2_{start_date}_{end_date}.csv"
+    data = Dataset(path.join(data_folder, data_filename))
+    hourly_NO2 = data.df.set_index('date', inplace=False)
+    # Columns suffix
+    hourly_NO2 = hourly_NO2.rename(columns={c: c+'_NO2' for c in hourly_NO2.columns})
+    dfs.append(hourly_NO2)
+    
+if propagation_approach == "multi" or (propagation_approach=="single" and species=="PM10"):
+    # Load PM10 data
+    data_filename = f"LAQN_PM10_{start_date}_{end_date}.csv"
+    data = Dataset(path.join(data_folder, data_filename))
+    hourly_PM10 = data.df.set_index('date', inplace=False)
+    # Columns suffix
+    hourly_PM10 = hourly_PM10.rename(columns={c: c+'_PM10' for c in hourly_PM10.columns})
+    dfs.append(hourly_PM10)
+# Concatenate species dataframes
+hourly_df = pd.concat(dfs, axis=1)
 
-hourly_df = data.df.set_index('date', inplace=False)
 print(f"Hourly dataframe shape: {hourly_df.shape}")
 
 # Get the training set (2011 to 2019)
@@ -29,26 +51,37 @@ complete_subset_train, column_names_train = get_complete_subset(
                   (hourly_df.index < '2019-01-01')], 
     num_valid_values=500)
 print("Complete training subset shape:", complete_subset_train.shape)
-# print("Stations:", column_names_train)
+print("Stations:", column_names_train)
 print(f"Start: {complete_subset_train.index.min()} End: {complete_subset_train.index.max()}")
-train_set = complete_subset_train.copy()
+
 # Get the test set (between the end of training set and 2020)
 print("Obtaining test subset...")
 df_test = hourly_df.loc[(hourly_df.index > complete_subset_train.index.max()) & 
                         (hourly_df.index < '2020-01-01')]
 complete_subset_test, column_names_test = get_complete_subset(df_test, num_valid_values=500)
 print("Complete test subset shape:", complete_subset_test.shape)
-# print("Stations:", column_names_test)
+print("Stations:", column_names_test)
 print(f"Start: {complete_subset_test.index.min()} End: {complete_subset_test.index.max()}")
-test_set = complete_subset_test.copy()
+
 # Get the COVID set between 26 March 2020 and 1 June 2020 (lockdown)
 print("Obtaining COVID test subset...")
 lockdown_df = hourly_df.loc[(hourly_df.index >= '2020-03-26') & (hourly_df.index <= '2020-06-01')]
 complete_subset_covid, column_names_covid = get_complete_subset(lockdown_df, num_valid_values=500)
 print("Complete test_covid subset shape:", complete_subset_covid.shape)
-# print("Stations:", column_names_covid)
+print("Stations:", column_names_covid)
 print(f"Start: {complete_subset_covid.index.min()} End: {complete_subset_covid.index.max()}")
-test_set_covid = complete_subset_covid.copy() # Use all of the lockdown data as the test set
+
+# Identify the index of the first column of the species of interest
+first_index_test = [i for i, s in enumerate(column_names_test) if species in s][0]
+first_index_covid = [i for i, s in enumerate(column_names_covid) if species in s][0]
+
+# Normalise the data across both species
+normalised_train, mins_train, maxs_train = normalise_data(complete_subset_train)
+normalised_test, mins_test, maxs_test = normalise_data(complete_subset_test)
+normalised_covid, mins_covid, maxs_covid = normalise_data(complete_subset_covid)
+print("Normalised train data shape:", normalised_train.shape)
+print("Normalised test data shape:", normalised_test.shape)
+print("Normalised COVID test data shape:", normalised_covid.shape)
 
 # Prepare DataFrames to store the results
 df_missing_proportions = pd.DataFrame(columns=['random_seed', 'train_missing%', 'test_missing%', 'COVID_missing%'])
@@ -67,9 +100,9 @@ for random_seed in tqdm(random_seeds, desc="Imputing with random seeds"):
     max_gap_length = 20
 
     # Call the introduce_gaps function for training, test, and COVID sets
-    gap_indices_train, true_values_train, subset_train = introduce_gaps(train_set, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
-    gap_indices_test, true_values_test, subset_test = introduce_gaps(test_set, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
-    gap_indices_test_covid, true_values_test_covid, subset_test_covid = introduce_gaps(test_set_covid, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
+    gap_indices_train, true_values_train, subset_train = introduce_gaps(normalised_train, species=species, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
+    gap_indices_test, true_values_test, subset_test = introduce_gaps(normalised_test, species=species, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
+    gap_indices_test_covid, true_values_test_covid, subset_test_covid = introduce_gaps(normalised_covid, species=species, proportion=gap_proportion, max_period_length=max_gap_length, seed=random_seed)
 
     # Compute missing data proportions
     train_missing_pct = (subset_train.isna().sum().sum() / subset_train.size) * 100
@@ -245,29 +278,46 @@ for random_seed in tqdm(random_seeds, desc="Imputing with random seeds"):
     plt.savefig(path.join(output_folder, f'threshold_vs_rmse_seed_{random_seed}.png'))
     plt.close()
 
-    # Save the scatter plots
+    # Scatter plot - test set
     x = np.arange(filled_data_test.max().max())
     plt.figure(figsize=(6, 6))
     plt.scatter(true_values_test, propagated_values_test)
     plt.plot(x, x, color='black', linestyle='--', label='1:1 Line')
     plt.title(f"test set")
-    plt.xlabel(rf'true {species} concentration ($\mu g/m^3$)')  
-    plt.ylabel(rf'propagated {species} concentration ($\mu g/m^3$)')  
+    plt.xlabel(rf'true {species} concentration (normalised values)')  
+    plt.ylabel(rf'propagated {species} concentration (normalised values)')  
     plt.legend()
     plt.savefig(path.join(output_folder, f'test_scatter_{random_seed}.png'))
     plt.close()
 
+    # Scatter plot - COVID test set
+    x_covid = np.arange(filled_data_covid.max().max())
+    plt.figure(figsize=(5, 5))
+    plt.scatter(true_values_test_covid, propagated_values_covid)
+    plt.plot(x_covid, x_covid, color='black', linestyle='--', label='1:1 Line')
+    plt.title(f"COVID test set")
+    plt.xlabel(rf'true {species} concentration (normalised values)')  
+    plt.ylabel(rf'propagated {species} concentration (normalised values)')  
+    plt.legend()
+    plt.savefig(path.join(output_folder, f'covid_scatter_{random_seed}.png'))
+    plt.close()
+
+    # Undo data normalisation for timeseries plotting
+    linear_test = denormalise_data(filled_data_test, mins_test, maxs_test)
+    linear_covid = denormalise_data(filled_data_covid, mins_covid, maxs_covid)
+
     # Time series plot for test set
     plt.figure(figsize=(12, 6))
-    column = column_names_test[0]
-    plt.plot(filled_data_test.index, filled_data_test[column], label=f"Linear model", alpha=1, color="m")
-    # Replace graph-propagated values into the filled data from polynomial interpolation
-    graph_prop = filled_data_test.copy()
+    column = column_names_test[first_index_test]
+    plt.plot(linear_test.index, linear_test[column], label=f"Linear model", alpha=1, color="m")
+    # Replace graph-propagated values into the filled data from linear interpolation
+    graph_test = filled_data_test.copy()
     for index, value in zip(gap_indices_test, propagated_values_test):
-        graph_prop.iloc[index[0], index[1]] = value
-    plt.plot(graph_prop.index, graph_prop[column], label=f"Graph propagation", alpha=1, color="tab:orange")
+        graph_test.iloc[index[0], index[1]] = value
+    graph_test = denormalise_data(graph_test, mins_test, maxs_test)
+    plt.plot(graph_test.index, graph_test[column], label=f"Graph propagation", alpha=1, color="tab:orange")
     # True values
-    plt.plot(test_set.index, test_set[column], label="True values", alpha=1, color="tab:blue")
+    plt.plot(complete_subset_test.index, complete_subset_test[column], label="True values", alpha=1, color="tab:blue")
     # Add labels and legend
     plt.xlabel('Date')
     plt.ylabel(f'{species} concentration ($\mu g/m^3$)')
@@ -276,27 +326,17 @@ for random_seed in tqdm(random_seeds, desc="Imputing with random seeds"):
     plt.savefig(path.join(output_folder, f'test_timeseries_{random_seed}.png'))
     plt.close()
 
-    # Repeat for COVID test set
-    x_covid = np.arange(filled_data_covid.max().max())
-    plt.figure(figsize=(5, 5))
-    plt.scatter(true_values_test_covid, propagated_values_covid)
-    plt.plot(x_covid, x_covid, color='black', linestyle='--', label='1:1 Line')
-    plt.title(f"COVID test set")
-    plt.xlabel(rf'true {species} concentration ($\mu g/m^3$)')  
-    plt.ylabel(rf'propagated {species} concentration ($\mu g/m^3$)')  
-    plt.legend()
-    plt.savefig(path.join(output_folder, f'covid_scatter_{random_seed}.png'))
-    plt.close()
-
+    # Time series plot for COVID test set
     plt.figure(figsize=(12, 6))
-    column = column_names_test[0]
-    plt.plot(filled_data_test.index, filled_data_test[column], label=f"Linear model", alpha=1, color="m")
+    column = column_names_covid[first_index_covid]
+    plt.plot(linear_covid.index, linear_covid[column], label=f"Linear model", alpha=1, color="m")
     # Replace graph-propagated values into the filled data from polynomial interpolation
-    graph_prop = filled_data_test.copy()
-    for index, value in zip(gap_indices_test, propagated_values_test):
-        graph_prop.iloc[index[0], index[1]] = value
-    plt.plot(graph_prop.index, graph_prop[column], label=f"Graph propagation", alpha=1, color="tab:orange")
-    plt.plot(test_set.index, test_set[column], label="True values", alpha=1, color="tab:blue")
+    graph_covid = filled_data_covid.copy()
+    for index, value in zip(gap_indices_test_covid, propagated_values_covid):
+        graph_covid.iloc[index[0], index[1]] = value
+    graph_covid = denormalise_data(graph_covid, mins_covid, maxs_covid)
+    plt.plot(graph_covid.index, graph_covid[column], label=f"Graph propagation", alpha=1, color="tab:orange")
+    plt.plot(complete_subset_covid.index, complete_subset_covid[column], label="True values", alpha=1, color="tab:blue")
     plt.xlabel('Date')
     plt.ylabel(f'{species} concentration ($\mu g/m^3$)')
     plt.title(f'{column} original data vs filled data')
